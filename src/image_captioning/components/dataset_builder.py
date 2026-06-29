09772ceeae847f802e9a715ed301ff033835f9d4
Mohammed Bilal <mohammed_b230057ee@nitc.ac.in>
2026-06-29 19:29:39 +0300

    refactor: simplify DatasetBuilder by removing unnecessary methods and comments

--- src/image_captioning/components/dataset_builder.py
+++ src/image_captioning/components/dataset_builder.py
@@ -6,12 +6,7 @@ import tensorflow as tf
 from image_captioning.utils.image_preprocessor import ImagePreprocessor
 
 
-
-
 class DatasetBuilder:
-    """
-    Builds a tf.data.Dataset for the Image Captioning model.
-    """
 
     def __init__(
         self,
@@ -20,18 +15,17 @@ class DatasetBuilder:
     ):
 
         self.batch_size = batch_size
-
-        self.preprocessor = ImagePreprocessor()
-
         self.vectorizer = vectorizer
 
     def _load_dataframe(self, caption_file):
+
         return pd.read_csv(caption_file)
 
     def _prepare_lists(self, dataframe, image_dir):
+
         image_paths = [
-            str(Path(image_dir) / image_name)
-            for image_name in dataframe["image_name"]
+            str(Path(image_dir) / img)
+            for img in dataframe["image_name"]
         ]
 
         captions = dataframe["caption"].tolist()
@@ -39,38 +33,10 @@ class DatasetBuilder:
         return image_paths, captions
 
     def _process(self, image_path, caption):
-        """
-        Python function executed through tf.py_function.
-        """
 
-        if isinstance(image_path, bytes):
-            image_path = image_path.decode("utf-8")
-        elif isinstance(image_path, tf.Tensor):
-            image_path = image_path.numpy().decode("utf-8")
+        image = ImagePreprocessor.preprocess(image_path)
 
-        if isinstance(caption, bytes):
-            caption = caption.decode("utf-8")
-        elif isinstance(caption, tf.Tensor):
-            caption = caption.numpy().decode("utf-8")
-
-        image = self.preprocessor.preprocess(image_path)
-
-        caption = tf.cast(
-            self.vectorizer.vectorize([caption])[0],
-            tf.int32,
-        )
-
-        return image, caption
-
-    def _tf_process(self, image_path, caption):
-        image, caption = tf.py_function(
-            self._process,
-            [image_path, caption],
-            [tf.float32, tf.int32],
-        )
-
-        image.set_shape((299, 299, 3))
-        caption.set_shape((25,))
+        caption = self.vectorizer.vectorize(tf.expand_dims(caption, 0))[0]
 
         return image, caption
 
@@ -79,13 +45,8 @@ class DatasetBuilder:
         caption_file,
         image_dir,
     ):
-        """
-        Returns:
-            tf.data.Dataset
-        """
 
         dataframe = self._load_dataframe(caption_file)
-
 
         image_paths, captions = self._prepare_lists(
             dataframe,
@@ -100,7 +61,7 @@ class DatasetBuilder:
         )
 
         dataset = dataset.map(
-            self._tf_process,
+            self._process,
             num_parallel_calls=tf.data.AUTOTUNE,
         )
 
