4aaeef9a2efafd77167dde26c69eb9880c4215a8
Mohammed Bilal <mohammed_b230057ee@nitc.ac.in>
2026-06-29 18:13:38 +0300

    feat: update requirements and setup for versioning and package management

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="image_captioning",
-    version="0.0.1",
+    version="1.0.0",
     author="Mohammed Bilal Shihabudeen",
     author_email="mohammedbilal2396@gmail.com",
+    description="End-to-End Transformer-based Image Captioning System",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    include_package_data=True,
+    python_requires=">=3.10",
 )
